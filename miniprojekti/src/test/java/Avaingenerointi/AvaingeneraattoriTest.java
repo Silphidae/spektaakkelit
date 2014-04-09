0b1e6375de5ab3ed559e82ca0252b512db0c62ff
noita <o.a.raty@gmail.com>
2014-04-09 18:59:59 +0300

    testejä avaingeneraattorille

--- miniprojekti/src/test/java/Avaingenerointi/AvaingeneraattoriTest.java
+++ miniprojekti/src/test/java/Avaingenerointi/AvaingeneraattoriTest.java
@@ -6,6 +6,8 @@ import Database.Database;
 import Database.MockDatabase;
 import Syotetarkistus.Syotetarkastaja;
 import domain.Article;
+import domain.Book;
+import domain.InProceedings;
 import domain.Kentta;
 import domain.Viite;
 import junit.framework.TestCase;
@@ -42,7 +44,7 @@ public class AvaingeneraattoriTest extends TestCase{
         assertEquals("SuEt", lyhenne);
     }
     
-    public void testUseitaSamojaArticleja() {
+    public void testUseitaSamojaArticle() {
         Viite v = luoTestiArticle("Etunimi Sukunimi", "1999");
         db.insertEntry(v);
         Viite v2 = luoTestiArticle("Etunimi Sukunimi", "1999");
@@ -54,7 +56,31 @@ public class AvaingeneraattoriTest extends TestCase{
         assertEquals("SuEt1999-2", v3.getViiteavain());
     }
     
-    public void testPalautuuOdotettuAvainArticlelle() {
+    public void testUseitaSamojaBook() {
+        Viite v = luoTestiBook("Etunimi Sukunimi", null, "1999");
+        db.insertEntry(v);
+        Viite v2 = luoTestiBook("Etunimi Sukunimi", "Toinen Nimi", "1999");
+        db.insertEntry(v2);
+        Viite v3 = luoTestiBook(null, "Etunimi Sukunimi", "1999");
+        db.insertEntry(v3);
+        
+        assertEquals("SuEt1999-1", v2.getViiteavain());
+        assertEquals("SuEt1999-2", v3.getViiteavain());
+    }
+    
+    public void testUseitaSamojaInProceedings() {
+        Viite v = luoTestiInProceedings("Etunimi Sukunimi", "1999");
+        db.insertEntry(v);
+        Viite v2 = luoTestiInProceedings("Etunimi Sukunimi", "1999");
+        db.insertEntry(v2);
+        Viite v3 = luoTestiInProceedings("Etunimi Sukunimi", "1999");
+        db.insertEntry(v3);
+        
+        assertEquals("SuEt1999-1", v2.getViiteavain());
+        assertEquals("SuEt1999-2", v3.getViiteavain());
+    }
+    
+    public void testPalautuuOdotettuAvainArticle() {
         Viite v = luoTestiArticle("Joku Nimi", "2000");
         assertEquals("NiJo2000", v.getViiteavain());
         
@@ -62,8 +88,41 @@ public class AvaingeneraattoriTest extends TestCase{
         assertEquals("ToJo1567", v2.getViiteavain());
     }
     
+    public void testPalautuuOdotettuAvainBook() {
+        Viite v = luoTestiBook("Joku Nimi", "Toinen Nimi", "2000");
+        assertEquals("NiJo2000", v.getViiteavain());
+        
+        Viite v2 = luoTestiBook(null, "Joku Toinen", "1567");
+        assertEquals("ToJo1567", v2.getViiteavain());
+    }
+    
+    public void testPalautuuOdotettuAvainInProceedings() {
+        Viite v = luoTestiInProceedings("Joku Nimi", "2000");
+        assertEquals("NiJo2000", v.getViiteavain());
+        
+        Viite v2 = luoTestiInProceedings("Joku Toinen", "1567");
+        assertEquals("ToJo1567", v2.getViiteavain());
+    }
+    
     public Viite luoTestiArticle(String nimi, String vuosi) {
         Viite v = new Article(tarkastaja);
+        v.lisaaKentta(Kentta.author, nimi);
+        v.lisaaKentta(Kentta.year, vuosi);
+        v.lisaaViiteavain(generaattori.luoAvain(v));
+        return v;
+    }    
+    
+    public Viite luoTestiBook(String author, String editor, String vuosi) {
+        Viite v = new Book(tarkastaja);
+        if (editor!=null) v.lisaaKentta(Kentta.editor, editor);
+        if (author!=null) v.lisaaKentta(Kentta.author, author);
+        v.lisaaKentta(Kentta.year, vuosi);
+        v.lisaaViiteavain(generaattori.luoAvain(v));
+        return v;
+    } 
+    
+    public Viite luoTestiInProceedings(String nimi, String vuosi) {
+        Viite v = new InProceedings(tarkastaja);
         v.lisaaKentta(Kentta.author, nimi);
         v.lisaaKentta(Kentta.year, vuosi);
         v.lisaaViiteavain(generaattori.luoAvain(v));
