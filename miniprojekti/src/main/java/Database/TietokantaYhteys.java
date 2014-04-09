2c6b716477c4f50ce5966977154874c4cb4ff10c
Outsou <otto.hantula@hotmail.com>
2014-04-09 18:38:40 +0300

    tiedosto tunnuksille

--- miniprojekti/src/main/java/Database/TietokantaYhteys.java
+++ miniprojekti/src/main/java/Database/TietokantaYhteys.java
@@ -1,6 +1,9 @@
 package Database;
 
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,28 +12,51 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 
-
 public class TietokantaYhteys {
+    
+    private static String asetustiedosto = "tietokanta.txt";
+    private static String kayttaja = "";
+    private static String salasana = "";
 
     public static Connection getYhteys() throws NamingException, SQLException {
         //ssh -L 5432:localhost:5432 käyttäjätunnus@users.cs.helsinki.fi
-        
+
+        File f = new File(asetustiedosto);
+        if (f.exists()) {
+            haeTunnukset();
+        }
+
         BasicDataSource ds = new BasicDataSource();
         ds.setUrl("jdbc:postgresql://localhost/ottohant");
-        ds.setUsername("korvaa käyttäjällä");
-        ds.setPassword("korvaa salasanalla");
+        ds.setUsername(kayttaja);
+        ds.setPassword(salasana);
         ds.setDriverClassName("org.postgresql.Driver");
         ds.setRemoveAbandoned(true);
         ds.setRemoveAbandonedTimeout(30);
         ds.setMaxActive(100);
         ds.setMaxIdle(30);
         ds.setMaxWait(1000);
-        
+
         DataSource yhteysVarasto = ds;
 
         Connection yhteys = yhteysVarasto.getConnection();
 
         return yhteys;
+    }
+
+    private static void haeTunnukset() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(asetustiedosto));
+            
+            String line = br.readLine();    
+            kayttaja = line;
+            
+            line = br.readLine();
+            salasana = line;
+            
+            br.close();
+        } catch (IOException iOException) {
+        }
     }
 
 }
