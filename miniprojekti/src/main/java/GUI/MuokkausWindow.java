cf71de162504f96c80f1f673f0b096960f08323d
Outsou <otto.hantula@hotmail.com>
2014-04-22 22:57:23 +0300

    Tallenna nappi lisätty

--- miniprojekti/src/main/java/GUI/MuokkausWindow.java
+++ miniprojekti/src/main/java/GUI/MuokkausWindow.java
@@ -31,6 +31,7 @@ public class MuokkausWindow extends javax.swing.JFrame {
 
         lomake = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -52,6 +53,8 @@ public class MuokkausWindow extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("Tallenna");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -62,6 +65,8 @@ public class MuokkausWindow extends javax.swing.JFrame {
                     .addComponent(lomake, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1)))
                 .addContainerGap())
         );
@@ -71,7 +76,9 @@ public class MuokkausWindow extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(lomake, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addContainerGap())
         );
 
@@ -86,6 +93,7 @@ public class MuokkausWindow extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JPanel lomake;
     // End of variables declaration//GEN-END:variables
 }
