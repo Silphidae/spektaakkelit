54ba645b0e517acd67b65ee2f3481c7913bb182c
Ansku <ansku@reba-Satellite-Pro-A200.(none)>
2014-04-24 22:47:56 +0300

    tagin poisto paivittyy muokkausnakymaan

--- miniprojekti/src/main/java/GUI/MuokkausWindow.java
+++ miniprojekti/src/main/java/GUI/MuokkausWindow.java
@@ -167,11 +167,12 @@ public class MuokkausWindow extends javax.swing.JFrame {
         ArrayList<String> tagit = new ArrayList<>();
 
         Component[] lomakkeenSisalto = lomake.getComponents();
+        JList tagilista = null;
 
         for (Component kentta : lomakkeenSisalto) {
             if (kentta instanceof JList) {
-                JList lista = (JList) kentta;
-                for (Object o : lista.getSelectedValuesList()) {
+                tagilista = (JList) kentta;
+                for (Object o : tagilista.getSelectedValuesList()) {
                     tagit.add(o.toString());
 
                 }
@@ -189,10 +190,15 @@ public class MuokkausWindow extends javax.swing.JFrame {
             for (String tagi : tagit) {
                 engine.removeTagi(ckey, tagi);
             }
+            paivitaTagit(tagilista);
             mainWindow.paivitaTagit();
         }
     }//GEN-LAST:event_jButton3ActionPerformed
-
+    public void paivitaTagit(JList lista) {
+        ArrayList<String> tagit = engine.getTagsByViite(ckey);
+        String[] sisalto = tagit.toArray(new String[tagit.size()]);
+        lista.setListData(sisalto);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
